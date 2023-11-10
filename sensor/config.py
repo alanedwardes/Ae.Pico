6af3957bec9393e7c30a98a1f2cf372e8bcade2c
Alan Edwardes <alan@alanedwardes.com>
2023-11-10 20:45:49 +0000

    Create config.py

---
+++ sensor/config.py
@@ -0,0 +1,18 @@
+wifi = dict(
+    ssid = "ssid",
+    key = "key",
+    host = "hostname"
+)
+
+hass = dict(
+    url = "http://localhost",
+    token = "<token>"
+)
+
+motion_sensor = dict(
+    friendly_name = "Test Motion",
+    name = "test_motion",
+    timeout_ms = 900_000,
+    pin = 22
+)
+
