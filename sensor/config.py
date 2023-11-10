af7203c5a806410c2f10f409eccf2a2f7a005fa3
Alan Edwardes <alan@alanedwardes.com>
2023-11-10 23:26:08 +0000

    Update config.py

--- sensor/config.py
+++ sensor/config.py
@@ -10,9 +10,21 @@ hass = dict(
 )
 
 motion_sensor = dict(
+    enabled = True,
     friendly_name = "Test Motion",
     name = "test_motion",
     timeout_ms = 900_000,
     pin = 22
 )
 
+bme280_sensor = dict(
+    enabled = True,
+    scl_pin = 1,
+    sda_pin = 0,
+    temp_friendly_name = "Test Temperature",
+    temp_name = "test_temperature",
+    pressure_friendly_name = "Test Pressure",
+    pressure_name = "test_pressure",
+    humidity_friendly_name = "Test Humidity",
+    humidity_name = "test_humidity"
+)
