659641882cbf1bfcaa61573bd312f9c64c3f110b
Alan Edwardes <alan@alanedwardes.com>
2023-11-10 23:24:55 +0000

    Update sensor.py

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -4,11 +4,20 @@ import time
 import ujson
 import urequests
 import gc
+import bme280
 import config
 
+# Init LED
 led = Pin("LED", Pin.OUT)
-motion = Pin(config.motion_sensor['pin'], Pin.IN)
 
+# Init motion sensor
+motion = Pin(config.motion_sensor['pin'], Pin.IN) if config.motion_sensor['enabled'] else None
+
+# Init BME280
+i2c = machine.I2C(0, scl=config.bme280_sensor['scl_pin'], sda=config.bme280_sensor['sda_pin']) if config.bme280_sensor['enabled'] else None
+bme = bme280.BME280(i2c=i2c) if config.bme280_sensor['enabled'] else None
+
+# Init WLAN
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.config(pm = 0xa11140, hostname = config.wifi['host'])
@@ -17,37 +26,41 @@ def connect_to_wifi():
     if not wlan.isconnected():
         print("Connecting to Wi-Fi...")
         wlan.connect(config.wifi['ssid'], config.wifi['key'])
+        print("Connect returned")
         while not wlan.isconnected():
             led.on()
             time.sleep(0.01)
             led.off()
     print("Connected to Wi-Fi")
     
-def send_update(motion_detected):
-    print("Sending update: " + str(motion_detected))    
+def send_update(state, unit, device_class, friendly_name, sensor):
+    print("Sending update for " + sensor + "=" + str(state))    
     
     data = {
-        "state": "on" if motion_detected else "off",
+        "state": state,
         "attributes": {
-            "device_class": "occupancy",
-            "friendly_name": config.motion_sensor['friendly_name']
+            "device_class": device_class,
+            "friendly_name": friendly_name
         }
     }
+    
+    if unit is not None:
+        data['attributes']['unit_of_measurement'] = unit
+        data['attributes']['state_class'] = "measurement"
 
     headers = {
         "Authorization": "Bearer " + config.hass['token'],
-        "Content-Type": "application/json"
+        "Content-Type": "application/json; charset=utf-8"
     }
 
-    response = urequests.post(config.hass['url'] + "/api/states/binary_sensor." + config.motion_sensor['name'], json=data, headers=headers)
-    if response.status_code != 200:
-        raise Exception("Status " + response.status_code + ": " + response.text)
+    response = urequests.post(config.hass['url'] + "/api/states/" + sensor, data=ujson.dumps(data).encode('utf-8'), headers=headers)
+    if not response.status_code in [200, 201]:
+        raise Exception("Status " + str(response.status_code) + ": " + response.text)
     
     print("Updated state")
 
 last_motion_ms = 0
 previous_motion_state = False
-
 def update_motion_sensor():
     global last_motion_ms, previous_motion_state
     
@@ -61,16 +74,45 @@ def update_motion_sensor():
     current_motion_state = sensor_value or time_since_last_motion_ms < config.motion_sensor['timeout_ms']
     
     if current_motion_state != previous_motion_state:
-        send_update(current_motion_state)
+        send_update("on" if current_motion_state else "off", None, "occupancy", config.motion_sensor['friendly_name'], "binary_sensor." + config.motion_sensor['name'])
 
     previous_motion_state = current_motion_state
+
+last_temp = 0
+last_pressure = 0
+last_humidity = 0
+def update_bme280_sensor():
+    global last_temp, last_pressure, last_humidity
+    
+    current_temp, current_pressure, current_humidity = bme.float_values()
+    
+    temp_diff = abs(last_temp - current_temp)
+    pressure_diff = abs(last_pressure - current_pressure)
+    humidity_diff = abs(last_humidity - current_humidity)
+    
+    if temp_diff > 0.1:
+        send_update(current_temp, "temperature", "°C", config.bme280_sensor['temp_friendly_name'], "sensor." + config.bme280_sensor['temp_name'])
+    
+    if pressure_diff > 0.25:
+        send_update(current_pressure, "atmospheric_pressure", "hPa", config.bme280_sensor['pressure_friendly_name'], "sensor." + config.bme280_sensor['pressure_name'])
+        
+    if humidity_diff > 1:
+        send_update(current_humidity, "humidity", "%", config.bme280_sensor['humidity_friendly_name'], "sensor." + config.bme280_sensor['humidity_name'])
+    
+    last_temp = current_temp
+    last_pressure = current_pressure
+    last_humidity = current_humidity
 
 def main_loop():  
     if not wlan.isconnected():
         print("Wi-Fi connection dropped. Reconnecting...")
         connect_to_wifi()
     
-    update_motion_sensor()
+    if config.motion_sensor['enabled']:
+        update_motion_sensor()
+        
+    if config.bme280_sensor['enabled']:
+        update_bme280_sensor()
 
 while True:
     led.off()
