87192a2c5cc147a51200916c99e63695d525749e
Alan Edwardes <alan@alanedwardes.com>
2024-08-17 23:02:39 +0100

    Removes prints.

--- management/cpython-main.py
+++ management/cpython-main.py
@@ -5,11 +5,8 @@ sys.path.insert(0,'../shims')
 import time
 import management
 
-print('test')
-
 server = management.ManagementServer()
 
 while True:
     server.update()
-    time.sleep(1)
-    print('hello')
+    time.sleep(0.01)
