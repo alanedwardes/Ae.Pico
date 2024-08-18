a96de45afea4803890b6d3bc0654f3703fbfda27
Alan Edwardes <alan@alanedwardes.com>
2024-08-18 21:42:13 +0100

    Completely re-works HTML generation to allow full utf8 support. Adds method to unescape URL encoding.

--- libraries/management.py
+++ libraries/management.py
@@ -7,6 +7,62 @@ import gc
 
 HEADER_TERMINATOR = b'\r\n'
 
+MINIMAL_CSS = b'<style>' \
+    b'form{display:inline;}' \
+    b'body{background-color:Canvas;color:CanvasText;color-scheme:light dark;font-family:sans-serif;}' \
+    b'</style>'
+
+BACK_LINK = b'<p><a href="/">Back</a></p>'
+
+ERROR_STATUS = b'HTTP/1.0 500 Internal Server Error' + HEADER_TERMINATOR
+OK_STATUS = b'HTTP/1.0 200 OK' + HEADER_TERMINATOR
+NOT_FOUND_STATUS = b'HTTP/1.0 404 Not Found' + HEADER_TERMINATOR
+UNAUTHORIZED_STATUS = b'HTTP/1.0 401 Unauthorized' + HEADER_TERMINATOR
+HTML_HEADER = b'Content-Type: text/html; charset=utf-8' + HEADER_TERMINATOR
+
+def unquote(string):
+    """unquote('abc%20def') -> b'abc def'.
+
+    Note: if the input is a str instance it is encoded as UTF-8.
+    This is only an issue if it contains unescaped non-ASCII characters,
+    which URIs should not.
+    """
+    if not string:
+        return b''
+
+    if isinstance(string, str):
+        string = string.encode('utf-8')
+
+    bits = string.split(b'%')
+    if len(bits) == 1:
+        return string
+
+    res = bytearray(bits[0])
+    append = res.append
+    extend = res.extend
+
+    for item in bits[1:]:
+        try:
+            append(int(item[:2], 16))
+            extend(item[2:])
+        except KeyError:
+            append(b'%')
+            extend(item)
+
+    return bytes(res)
+
+def parse_form(form):
+    data = {}
+
+    for item in form.split(b'&'):
+        item = item.replace(b'+', b' ')
+        parts = item.split(b'=')
+        key = unquote(parts[0])
+        value = unquote(parts[1])
+        data[key] = value
+    
+    return data
+
 def parse_headers(connection):
     headers = {}
     offset = 0
@@ -31,11 +87,7 @@ class IndexController:
     def route(self, method, path):
         return method == b'GET' and path == b'/'
     
-    def serve(self, headers, connection):
-        connection.write(b'HTTP/1.0 200 OK' + HEADER_TERMINATOR)
-        connection.write(b'Content-Type: text/html' + HEADER_TERMINATOR)
-        connection.write(HEADER_TERMINATOR)
-        
+    def serve(self, headers, connection):        
         statvfs = uos.statvfs("/")
         free_space = statvfs[0] * statvfs[3]
         free_memory = gc.mem_free() if hasattr(gc, 'mem_free') else 0
@@ -56,23 +108,26 @@ class IndexController:
             return rtc.datetime()
         
         KB = 1024
-        
-        connection.write(b'<style>body{form{display:inline}</style>')        
+
+        connection.write(OK_STATUS)
+        connection.write(HTML_HEADER)
+        connection.write(HEADER_TERMINATOR)
+        connection.write(MINIMAL_CSS)
         connection.write(b'<h1>Management Dashboard</h1>')
-        connection.write('<p>{}</p>'.format(uos.uname()).encode('utf-8'))
+        connection.write(b'<p>%s</p>' % str(uos.uname()).encode('utf-8'))
         connection.write(b'<ul>')
-        connection.write('<li>Uptime: {:.0f}s</li>'.format(uptime_ms / 1000).encode('utf-8'))
-        connection.write('<li>CPU frequency: {:.0f} MHz</li>'.format(machine.freq() / 1_000_000).encode('utf-8'))
-        connection.write('<li>CPU temperature: {:.0f} celcius</li>'.format(cpu_temp()).encode('utf-8'))
-        connection.write('<li>Unique ID: {}</li>'.format(unique_id()).encode('utf-8'))
-        connection.write('<li>Current time: {}</li>'.format(current_time()).encode('utf-8'))
+        connection.write(b'<li>Uptime: %.0f KB</li>' % (uptime_ms / 1000))
+        connection.write(b'<li>CPU frequency: %.0f KB</li>' % (machine.freq() / 1_000_000))
+        connection.write(b'<li>CPU temperature: %.0f celcius</li>' % (cpu_temp()))
+        connection.write(b'<li>Unique ID: %s</li>' % (unique_id().encode('utf-8')))
+        connection.write(b'<li>Current time: %s</li>' % str(current_time()).encode('utf-8'))
         connection.write(b'</ul>')
         connection.write(b'<h2>System</h2>')
         connection.write(b'<form action="reboot" method="post"><button>Reboot</button/></form>')
         connection.write(b'<h2>Memory</h2>')
-        connection.write('<p>Free Memory: {:.2f} KB</p>'.format(free_memory / KB).encode('utf-8'))
+        connection.write(b'<p>Free Memory: %.2f KB</p>' % (free_memory / KB))
         connection.write(b'<h2>Filesystem</h2>')
-        connection.write('<p>Free Space: {:.2f} KB</p>'.format(free_space / KB).encode('utf-8'))
+        connection.write(b'<p>Free Space: %.2f KB</p>' % (free_space / KB))
         connection.write(b'<h3>Files</h3>')
         connection.write(b'<table>')
         connection.write(b'<thead><tr><th>Name</th><th>Size</th><th>Actions</th></tr></thead>')
@@ -81,22 +136,22 @@ class IndexController:
         def write_file(parent, file):
             path = parent + file[0]
             connection.write(b'<tr>')
-            connection.write(b'<td>' + path + '</td>')
-            connection.write(b'<td>{:.2f} KB</td>'.format(file[3] / KB))
+            connection.write(b'<td>%s</td>' % (path))
+            connection.write(b'<td>%.2f KB</td>' % (file[3] / KB))
             connection.write(b'<td>')
-            connection.write(b'<form action="delete" method="post"><input type="hidden" name="filename" value="' + path + '"/><button>Delete</button/></form>')
-            connection.write(b'<form action="download" method="post"><input type="hidden" name="filename" value="' + path + '"/><button>Download</button/></form>')
+            connection.write(b'<form action="delete" method="post"><input type="hidden" name="filename" value="%s"/><button>Delete</button/></form>' % (path))
+            connection.write(b'<form action="download" method="post"><input type="hidden" name="filename" value="%s"/><button>Download</button/></form>' % (path))
             connection.write(b'</td>')
             connection.write(b'</tr>')
         
         def list_contents_recursive(start):
             for node in uos.ilistdir(start):
                 if node[1] == 0x4000:
-                    list_contents_recursive(start + node[0] + '/')
+                    list_contents_recursive(start + node[0] + b'/')
                 else:
                     write_file(start, node)
         
-        list_contents_recursive('')
+        list_contents_recursive(b'')
         
         connection.write(b'</tbody>')
         connection.write(b'</table>')
@@ -109,43 +164,31 @@ class DeleteController:
         return method == b'POST' and path == b'/delete'
     
     def serve(self, headers, connection):
-        connection.write(b'HTTP/1.0 200 OK' + HEADER_TERMINATOR)
-        connection.write(b'Content-Type: text/html' + HEADER_TERMINATOR)
-        connection.write(HEADER_TERMINATOR)
-        
         content_length = int(headers.get(b'content-length', '0'))
-        filename = connection.read(content_length).split(b'filename=')[1]
-        
-        try:
-            uos.unlink(filename)
-            connection.write(b'<p>Deleted "{}"</p>'.format(filename))
-        except Exception as e:
-            connection.write(b'<p>Error deleting "{}": {}</p>'.format(filename, e))
-            
-        connection.write(b'<p><a href="/">Back</a></p>')
+        form = parse_form(connection.read(content_length))
+        filename = form[b'filename']
+
+        uos.unlink(filename)
+        connection.write(OK_STATUS)
+        connection.write(HTML_HEADER)
+        connection.write(HEADER_TERMINATOR)
+        connection.write(MINIMAL_CSS)
+        connection.write(b'<p>Deleted %s</p>' % (filename))            
+        connection.write(BACK_LINK)
         
 class UploadController:    
     def route(self, method, path):
         return method == b'POST' and path == b'/upload'
     
     def serve(self, headers, connection):
-        connection.write(b'HTTP/1.0 200 OK' + HEADER_TERMINATOR)
-        connection.write(b'Content-Type: text/html' + HEADER_TERMINATOR)
-        connection.write(HEADER_TERMINATOR)
-        
-        content_length = int(headers.get(b'content-length', '0'))
-        
-        boundary = connection.readline()
-        
+        content_length = int(headers.get(b'content-length', '0'))        
+        boundary = connection.readline()        
         (headers_offset, content_headers) = parse_headers(connection)
-
-        filename = content_headers[b'content-disposition'].split(b'filename=')[1].split(b'"')[1]
-        
+        filename = content_headers[b'content-disposition'].split(b'filename=')[1].split(b'"')[1]        
         offset = headers_offset + len(boundary)
-
         content_size = content_length - offset - len(boundary) - len(HEADER_TERMINATOR) * 2
         
-        with open(filename, 'w') as f:
+        with open(filename, 'wb') as f:
             f.write(connection.read(content_size))
         
         # After the content there should always be a terminator
@@ -156,8 +199,12 @@ class UploadController:
         if connection.readline() != boundary[:len(boundary) - len(HEADER_TERMINATOR)] + b'--\r\n':
             raise Exception('Expected final boundary')
 
-        connection.write(b'<p>{} uploaded ({} bytes)'.format(filename, content_size))
-        connection.write(b'<p><a href="/">Back</a></p>')
+        connection.write(OK_STATUS)
+        connection.write(HTML_HEADER)
+        connection.write(HEADER_TERMINATOR)
+        connection.write(MINIMAL_CSS)
+        connection.write(b'<p>%s uploaded (%i bytes)' % (filename, content_size))
+        connection.write(BACK_LINK)
 
 class DownloadController:
     def route(self, method, path):
@@ -165,35 +212,31 @@ class DownloadController:
     
     def serve(self, headers, connection):        
         content_length = int(headers.get(b'content-length', '0'))
-        filename = connection.read(content_length).split(b'filename=')[1].decode('utf-8')
+        form = parse_form(connection.read(content_length))
+        filename = form[b'filename']
         
-        connection.write(b'HTTP/1.0 200 OK' + HEADER_TERMINATOR)
-        
-        try:
+        with open(filename, 'rb') as f:
             stat = uos.stat(filename)
-            connection.write(b'Content-Length: {}'.format(stat[6]) + HEADER_TERMINATOR)
+            connection.write(OK_STATUS)
+            connection.write(b'Content-Length: %i' % (stat[6]) + HEADER_TERMINATOR)
             connection.write(b'Content-Type: application/octet-stream' + HEADER_TERMINATOR)
-            connection.write(b'Content-Disposition: attachment; filename="{}"'.format(filename) + HEADER_TERMINATOR)
+            connection.write(b'Content-Disposition: attachment; filename="%s"' % (filename) + HEADER_TERMINATOR)
             connection.write(HEADER_TERMINATOR)
-            
-            with open(filename, 'r') as f:
-                connection.write(f.read())
-        
-        except Exception as e:
-            connection.write(b'HTTP/1.0 200 OK' + HEADER_TERMINATOR)
-            connection.write(b'Content-Type: text/html' + HEADER_TERMINATOR)
-            connection.write(HEADER_TERMINATOR)
-            
-            connection.write(b'<p>Error reading "{}": {}</p>'.format(filename, e))
-            connection.write(b'<p><a href="/">Back</a></p>')
+            connection.write(f.read())
             
 class RebootController:
     def route(self, method, path):
         return method == b'POST' and path == b'/reboot'
     
     def serve(self, headers, connection):
-        connection.write(b'HTTP/1.0 200 OK' + HEADER_TERMINATOR)
+        connection.write(OK_STATUS)
+        connection.write(HTML_HEADER)
         connection.write(HEADER_TERMINATOR)
+        connection.write(MINIMAL_CSS)
+        connection.write(b'<p>System will reboot in 5 seconds.</p>')
+        connection.write(BACK_LINK)
+        connection.close()
+        utime.sleep(5)
         machine.reset()
 
 class ManagementServer:   
@@ -228,15 +271,23 @@ class ManagementServer:
             
             # HTTP Basic Authentication
             if self.authorization_header and headers.get(b'authorization', None) != self.authorization_header:
-                connection.write(b'HTTP/1.0 401 Unauthorized' + HEADER_TERMINATOR)
+                connection.write(UNAUTHORIZED_STATUS)
                 connection.write(b'WWW-Authenticate: Basic realm="Management Server"' + HEADER_TERMINATOR)
+                connection.write(HTML_HEADER)
                 connection.write(HEADER_TERMINATOR)
+                connection.write(MINIMAL_CSS)
                 connection.write(b'<p>Unauthorized</p>' + HEADER_TERMINATOR)
             else:
                 self.__route(command[0], command[1], headers, connection)
             
         except Exception as e:
-            print('Error', e)
+            connection.write(ERROR_STATUS)
+            connection.write(HTML_HEADER)
+            connection.write(HEADER_TERMINATOR)
+            connection.write(MINIMAL_CSS)
+            connection.write(b'<p>Unhandled Exception: %s</p>' % type(e).__name__)
+            connection.write(b'<pre>%s</pre>' % str(e))
+            connection.write(BACK_LINK)
         
         finally:
             connection.close()
@@ -248,4 +299,9 @@ class ManagementServer:
                 controller.serve(headers, connection)
                 return
         
-        connection.write(b'404')
+        connection.write(NOT_FOUND_STATUS)
+        connection.write(HTML_HEADER)
+        connection.write(HEADER_TERMINATOR)
+        connection.write(MINIMAL_CSS)
+        connection.write(b'<p>Not found</p>')
+        connection.write(BACK_LINK)
--- shims/uos.py
+++ shims/uos.py
@@ -1,3 +1,5 @@
+import os
+
 def statvfs(path):
     return (0,0,0,0,0,0)
 
@@ -5,4 +7,14 @@ def uname():
     return 'cypython'
 
 def ilistdir(path):
-    return []
+    with os.scandir(b'./' + path) as it:
+        for entry in it:
+            stat = entry.stat()
+            yield (entry.name, 0x4000 if entry.is_dir() else 0, None, stat.st_size)
+
+def stat(path):
+    stat = os.stat(path)
+    return (None, None, None, None, None, None, stat.st_size)
+
+def unlink(path):
+    os.unlink(path)
