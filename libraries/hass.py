366b3c55d00ed81a7afd4056ad2f7539db6b37a8
Alan Edwardes <alan@alanedwardes.com>
2024-09-09 17:32:00 +0100

    Adds render_template and set_time methods to hass.py

--- libraries/hass.py
+++ libraries/hass.py
@@ -32,5 +32,29 @@ class Hass:
         }
 
         response = urequests.post(self.url + "/api/states/" + sensor, data=ujson.dumps(data).encode('utf-8'), headers=headers, timeout=5)
+        self.ensure_success_status_code(response)
+    
+    def ensure_success_status_code(self, response):
         if not response.status_code in [200, 201]:
             raise Exception("Status " + str(response.status_code) + ": " + response.text)
+        return response
+    
+    def render_template(self, template):
+        data = {
+            "template": template
+        }
+        
+        headers = {
+            "Authorization": "Bearer " + self.token,
+            "Content-Type": "application/json; charset=utf-8"
+        }
+        
+        response = urequests.post(self.url + "/api/template", data=ujson.dumps(data).encode('utf-8'), headers=headers, timeout=5)
+        self.ensure_success_status_code(response)
+        return response.text
+
+    def set_time(self):
+        now = self.render_template("{{ now().timestamp() | timestamp_custom('%Y,%m,%d,%w,%H,%M,%S,%f') }}")
+        ts = tuple(map(int, now.split(',')))
+        import machine
+        machine.RTC().datetime(ts)
