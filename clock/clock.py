7cf5accb0d8789a4bc129ed8b9663150e7d055ee
Alan Edwardes <alan@alanedwardes.com>
2024-09-09 18:50:04 +0100

    Adds WiFi signal strength to clock.

--- clock/clock.py
+++ clock/clock.py
@@ -1,11 +1,14 @@
 import utime
+import network
 
 class Clock:
     def __init__(self, display):
         self.display = display
+        self.wlan = network.WLAN(network.STA_IF)
         
         self.white = self.display.create_pen(255, 255, 255)
         self.grey = self.display.create_pen(128, 128, 128)
+        self.hidden = self.display.create_pen(32, 32, 32)
         self.black = self.display.create_pen(0, 0, 0)
         self.orange = self.display.create_pen(255, 165, 0)
 
@@ -17,7 +20,7 @@ class Clock:
     def draw_text(self, text, scale, y):
         width = self.display.measure_text(text, scale)
         height = scale * 20
-        self.display.set_thickness(scale * 2)
+        self.display.set_thickness(int(scale * 2))
 
         x = int(self.display_half_width - width * 0.5)
         
@@ -48,11 +51,16 @@ class Clock:
         y += 16
         
         self.display.set_pen(self.orange)
-        y += self.draw_rectangle(256, 8, y)
+        y += self.draw_rectangle(280, 8, y)
         
         y += 16
         
         self.display.set_pen(self.grey)
         y += self.draw_text("%02i/%02i/%02i" % (now[2], now[1], now[0]), 1, y)
+        
+        y += 16
+        
+        self.display.set_pen(self.hidden)
+        y += self.draw_text("%i dB" % (self.wlan.status('rssi')), 1, y)
         
         self.display.update()
