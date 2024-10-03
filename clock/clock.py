8fec0f49f67776f9cfc31aa2f2fadc090e3b555b
Alan Edwardes <alan@alanedwardes.com>
2024-10-03 20:07:55 +0100

    Legibility updates for clock.

--- clock/clock.py
+++ clock/clock.py
@@ -14,13 +14,11 @@ class Clock:
 
         self.display_width, self.display_height = self.display.get_bounds()
         self.display_half_width = self.display_width * 0.5
-
-        self.display.set_font("sans")
     
     def draw_text(self, text, scale, y):
         width = self.display.measure_text(text, scale)
         height = scale * 20
-        self.display.set_thickness(int(scale * 2))
+        self.display.set_thickness(int(scale * 3))
 
         x = int(self.display_half_width - width * 0.5)
         
@@ -28,7 +26,7 @@ class Clock:
         
         self.display.text(text, x, int(y + half_height), scale=scale)
         
-        return height
+        return int(height)
 
     def draw_rectangle(self, width, height, y):
         x = int(self.display_half_width - width * 0.5)
@@ -38,6 +36,7 @@ class Clock:
         return height
 
     def update(self):
+        self.display.set_font("sans")
         self.display.set_pen(self.black)
         self.display.clear()
 
@@ -46,21 +45,21 @@ class Clock:
         y = 64
         
         self.display.set_pen(self.white)
-        y += self.draw_text("%02i:%02i:%02i" % (now[3], now[4], now[5]), 2, y)
+        y += self.draw_text("%02i:%02i:%02i" % (now[3], now[4], now[5]), 2.25, y)
     
         y += 16
         
         self.display.set_pen(self.orange)
-        y += self.draw_rectangle(280, 8, y)
+        y += self.draw_rectangle(320, 8, y)
         
         y += 16
         
         self.display.set_pen(self.grey)
-        y += self.draw_text("%02i/%02i/%02i" % (now[2], now[1], now[0]), 1, y)
+        y += self.draw_text("%02i/%02i/%02i" % (now[2], now[1], now[0]), 1.5, y)
         
-        y += 16
-        
+        self.display.set_font("bitmap8")
         self.display.set_pen(self.hidden)
-        y += self.draw_text("%i dB" % (self.wlan.status('rssi')), 1, y)
+        self.display.set_thickness(1)
+        self.display.text("%i dB" % (self.wlan.status('rssi')), 10, 220, scale=1)
         
         self.display.update()
