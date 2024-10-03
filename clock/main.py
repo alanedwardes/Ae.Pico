75d2876cc961c0213c784460c5d32f89ced2c335
Alan Edwardes <alan@alanedwardes.com>
2024-10-03 17:26:59 +0100

    Replaces hass implementaton with remote clock in clock app.

--- clock/main.py
+++ clock/main.py
@@ -6,39 +6,25 @@ import clock
 import picographics
 import utime
 import config
+import remotetime
 from wifi import WiFi
-from hass import Hass
 
 display = picographics.PicoGraphics(display=picographics.DISPLAY_PICO_DISPLAY_2, pen_type=picographics.PEN_P4, rotate=config.clock['rotate'])
 display.set_backlight(1)
 
 wifi = WiFi(config.wifi['host'], config.wifi['ssid'], config.wifi['key'])
-hass = Hass(config.hass['url'], config.hass['token'])
+time = remotetime.RemoteTime(config.clock['endpoint'], config.clock['update_time_ms'])
 
 server = management.ManagementServer()
 
 clock = clock.Clock(display)
-
-last_updated_time = None
-def update_time():
-    global last_updated_time
-    
-    if last_updated_time is None or utime.ticks_diff(utime.ticks_ms(), last_updated_time) > config.clock['update_time_ms']:
-        try:
-            hass.set_time()
-            last_updated_time = utime.ticks_ms()
-            print('Time updated')
-        except Exception as e:
-            print('Error refreshing the time', e)
-        finally:
-            gc.collect()
 
 def main_loop():
     clock.update()
     wifi.update()
     
     if wifi.is_connected():
-        update_time()
+        time.update()
         server.update()
 
 wd = machine.WDT(timeout=8388)
