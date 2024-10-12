7c88361a07018228d85f2d6ff11425c36c0a7ead
Alan Edwardes <alan@alanedwardes.com>
2024-10-12 20:06:20 +0100

    Enhances example config.

--- thermostat/config.py
+++ thermostat/config.py
@@ -13,9 +13,14 @@ thermostat = dict(
     rotate = 0,
     entity_id = "<entity id>",
     occupancy_entity_id = "<optional entity id>",
-    current_temperature_entity_id = "<optional entity id>",
-    maximum_temperature_entity_id = "<optional entity id>",
-    current_precipitation_entity_id = "<optional entity id>"
+    middle_row = [
+        dict(entity_id = '<thermostat entity id>', attribute = 'current_temperature', label = 'now', format = lambda x: '%.1fc' % float(x), temperature = True),
+        dict(entity_id = '<thermostat entity id>', attribute = 'temperature', label = 'target', format = lambda x: '%.1fc' % float(x), temperature = True)
+    ],
+    bottom_row = [
+        dict(entity_id = '<current weather entity id>', label = 'now', format = lambda x: '%.0fc' % float(x), temperature = True),
+        dict(entity_id = '<current rain chance entity id>', label = 'rain', format = lambda x: '%.0f%%' % float(x))
+    ]
 )
 
 clock = dict(
