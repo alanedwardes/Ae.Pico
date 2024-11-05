44319c4084ca6e1cb485cdc0b1a592af84c474ae
Alan Edwardes <alan@alanedwardes.com>
2024-11-05 21:16:18 +0000

    Increases polling rate for buttons, adds debounce delay.

--- libraries/buttons.py
+++ libraries/buttons.py
@@ -1,9 +1,10 @@
 import asyncio
 
 class Buttons:
-    def __init__(self, polling_rate_ms = 100):
+    def __init__(self, polling_rate_ms = 10, debounce_ms = 100):
         self.bindings = []
         self.polling_rate_ms = polling_rate_ms
+        self.debounce_ms = debounce_ms
     
     def bind(self, pin, callback):
         self.bindings.append((pin, callback))
@@ -13,6 +14,7 @@ class Buttons:
             for pin, callback in self.bindings:
                 if pin.value() == 0:
                     await callback(pin)
+                    await asyncio.sleep_ms(self.debounce_ms)
             await asyncio.sleep_ms(self.polling_rate_ms)
             
     async def stop(self):
