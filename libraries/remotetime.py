615a85c5e1c73addfb1b924ed8f401be22844dd0
Alan Edwardes <alan@alanedwardes.com>
2024-11-23 22:44:56 +0000

    Removes support for non-standard HTTP time protocol.

--- libraries/remotetime.py
+++ libraries/remotetime.py
@@ -3,34 +3,22 @@ import utime
 import struct
 import socket
 import asyncio
-import binascii
 from collections import namedtuple
 
-URL_RE = re.compile(r'(ntp|http|https)://([A-Za-z0-9-\.]+)(?:\:([0-9]+))?(.+)?')
-URI = namedtuple('URI', ('protocol', 'hostname', 'port', 'path'))
+URL_RE = re.compile(r'ntp://([A-Za-z0-9-\.]+)(?:\:([0-9]+))?(.+)?')
+URI = namedtuple('URI', ('hostname', 'port', 'path'))
 
 def urlparse(uri):
     match = URL_RE.match(uri)
     if match:
-        protocol = match.group(1)
-        host = match.group(2)
-        port = match.group(3)
-        path = match.group(4)
+        host = match.group(1)
+        port = match.group(2)
+        path = match.group(3)
 
-        if protocol == 'https':
-            if port is None:
-                port = 443
-        elif protocol == 'http':
-            if port is None:
-                port = 80
-        elif protocol == 'ntp':
-            if port is None:
-                port = 123
-            pass
-        else:
-            raise ValueError('Scheme {} is invalid'.format(protocol))
+        if port is None:
+            port = 123
 
-        return URI(protocol, host.encode('ascii'), int(port), b'/' if path is None else path.encode('utf-8'))
+        return URI(host.encode('ascii'), int(port), b'/' if path is None else path.encode('utf-8'))
 
 class RemoteTime:
     def __init__(self, endpoint, update_time_ms, nic):
@@ -50,12 +38,6 @@ class RemoteTime:
         pass
 
     async def get_time(self):
-        if self.uri.protocol == 'ntp':
-            return await self.get_time_ntp()
-        elif self.uri.protocol == 'http' or self.uri.protocol == 'https':
-            return await self.get_time_http()
-
-    async def get_time_ntp(self):
         NTP_QUERY = bytearray(48)
         NTP_QUERY[0] = 0x1B
         addr = socket.getaddrinfo(self.uri.hostname, self.uri.port)[0][-1]
@@ -84,23 +66,6 @@ class RemoteTime:
         t = val - NTP_DELTA
         tm = utime.gmtime(t)
         return (tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0)
-
-    async def get_time_http(self):
-        reader, writer = await asyncio.open_connection(self.uri.hostname, self.uri.port, ssl = self.uri.port == 443)
-        writer.write(b'GET %s HTTP/1.0\r\nHost: %s\r\n\r\n' % (self.uri.path, self.uri.hostname))
-        await writer.drain()
-
-        header_prefix = b'Hora: '
-        
-        while True:
-            line = await reader.readline()
-            if not line:
-                raise Exception('Time header not found in response')
-
-            if line.startswith(header_prefix):
-                writer.close()
-                await writer.wait_closed()
-                return struct.unpack('HBBBBBBH', binascii.unhexlify(line[len(header_prefix):-2]))
     
     async def update_time(self):
         ts = await self.get_time()
--- tests/remotetime_tests.py
+++ tests/remotetime_tests.py
@@ -11,39 +11,7 @@ class TestRemoteTime(unittest.IsolatedAsyncioTestCase):
 
     nic = network.AbstractNIC
 
-    async def test_get_time_https(self):
-        rt = remotetime.RemoteTime('https://time.alanedwardes.com/', 300_000, self.nic)
-        ts = await rt.get_time()
-        self.assertEqual(300_000, rt.update_time_ms)
-        self.assertEqual(8, len(ts))
-
-        now = datetime.datetime.now()
-
-        self.assertEqual(now.year, ts[0])
-        self.assertEqual(now.month, ts[1])
-        self.assertEqual(now.day, ts[2])
-        self.assertEqual(now.isoweekday(), ts[3])
-        self.assertEqual(now.hour, ts[4])
-        self.assertEqual(now.minute, ts[5])
-        self.assertEqual(now.second, ts[6])
-
-    async def test_get_time_http(self):
-        rt = remotetime.RemoteTime('http://time.alanedwardes.com/', 300_000, self.nic)
-        ts = await rt.get_time()
-        self.assertEqual(300_000, rt.update_time_ms)
-        self.assertEqual(8, len(ts))
-
-        now = datetime.datetime.now()
-
-        self.assertEqual(now.year, ts[0])
-        self.assertEqual(now.month, ts[1])
-        self.assertEqual(now.day, ts[2])
-        self.assertEqual(now.isoweekday(), ts[3])
-        self.assertEqual(now.hour, ts[4])
-        self.assertEqual(now.minute, ts[5])
-        self.assertEqual(now.second, ts[6])
-
-    async def test_get_time_ntp(self):
+    async def test_get_time(self):
         rt = remotetime.RemoteTime('ntp://pool.ntp.org/', 300_000, self.nic)
         ts = await rt.get_time()
         self.assertEqual(300_000, rt.update_time_ms)
@@ -58,11 +26,6 @@ class TestRemoteTime(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(now.hour, ts[4])
         self.assertEqual(now.minute, ts[5])
         self.assertEqual(now.second, ts[6])
-
-    async def test_get_time_http_missing_header(self):
-        rt = remotetime.RemoteTime('https://alanedwardes.com/', 300_000, self.nic)
-        with self.assertRaises(Exception):
-            await rt.get_time()
 
 if __name__ == '__main__':
     unittest.main()
