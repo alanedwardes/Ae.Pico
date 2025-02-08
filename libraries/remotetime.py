0599f5d232b669aa8a3623077414ab9d8794c731
Alan Edwardes <alan@alanedwardes.com>
2025-02-08 13:22:57 +0000

    Adds receive offset for remotetime.py

--- libraries/remotetime.py
+++ libraries/remotetime.py
@@ -47,10 +47,14 @@ class RemoteTime:
         NTP_QUERY[0] = 0x1B
         addr = socket.getaddrinfo(self.uri.hostname, self.uri.port)[0][-1]
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        receive_milliseconds = 0
         try:
             s.settimeout(1)
             s.sendto(NTP_QUERY, addr)
+            sent_time = utime.ticks_ms()
             msg = s.recv(48)
+            # Time how long it takes to obtain a response and adjust
+            receive_milliseconds = utime.ticks_diff(utime.ticks_ms(), sent_time)
         finally:
             s.close()
         val = struct.unpack("!I", msg[40:44])[0]
@@ -71,12 +75,17 @@ class RemoteTime:
 
         t = val - NTP_DELTA
         
+        adjust_seconds = receive_milliseconds // 1000
+        adjust_milliseconds = receive_milliseconds % 1000
+        
         milliseconds = (val2 * 1000) // 0x100000000
         
         tm = utime.gmtime(t)
-        return (tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], milliseconds)
+        return (tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5] + adjust_seconds, milliseconds + adjust_milliseconds)
     
-    def calculate_time(self):
+    # Provides a method compatible with machine.RTC to obtain the time
+    # Provides millisecond resolution (whereas on some ports machine.RTC does not)
+    def datetime(self):
         total_milliseconds = self.last_timestamp[7] + utime.ticks_diff(utime.ticks_ms(), self.last_update)       
         total_seconds = self.last_timestamp[6] + (total_milliseconds // 1000)
         total_minutes = self.last_timestamp[5] + (total_seconds // 60)
@@ -86,6 +95,7 @@ class RemoteTime:
         seconds = total_seconds % 60
         minutes = total_minutes % 60
         hours = total_hours % 24
+        
         # (year, month, day, weekday, hours, minutes, seconds, subseconds)
         return (self.last_timestamp[0], self.last_timestamp[1], self.last_timestamp[2], self.last_timestamp[3], hours, minutes, seconds, milliseconds)
     
