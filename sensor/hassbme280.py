c38146a26aee8d55d8663ad08c135f41cdeaf5c7
Alan Edwardes <alan@alanedwardes.com>
2025-02-15 00:12:31 +0000

    Adds hassmotion, fixes problems with hassbme280.

--- sensor/hassbme280.py
+++ sensor/hassbme280.py
@@ -1,10 +1,12 @@
 import bme280
+import asyncio
+import machine
 import datapoint
 
 class HassBme280:
     def __init__(self, hass, scl, sda, temperature_config, pressure_config, humidity_config):
-        self.i2c = machine.I2C(0, scl, sda)
-        self.bme = bme280.BME280(self.i2c)
+        self.i2c = machine.I2C(0, scl=scl, sda=sda)
+        self.bme = bme280.BME280(i2c=self.i2c)
         self.hass = hass
         
         self.temperature_config = temperature_config
@@ -13,18 +15,16 @@ class HassBme280:
         
         # See https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
         # Only report value changes outside of accuracy tolerance
-        self.temperature = DataPoint(0.5)
-        self.pressure = DataPoint(1)
-        self.humidity = DataPoint(3)
+        self.temperature = datapoint.DataPoint(0.5)
+        self.pressure = datapoint.DataPoint(1)
+        self.humidity = datapoint.DataPoint(3)
 
-    def create():
+    CREATION_PRIORITY = 1
+    def create(provider):
         config = provider['config']['bme280']
-        return HassBme280(provider['hass.Hass'], config['scl'], config['sda'], config['temperature'], config['pressure'], config['humudity'])
+        return HassBme280(provider['hass.Hass'], config['scl'], config['sda'], config['temperature'], config['pressure'], config['humidity'])
     
     async def start(self):
-        while not self.nic.isconnected():
-            await asyncio.sleep_ms(100)
-        
         while True:
             current_temp, current_pressure, current_humidity = self.bme.float_values()
             
@@ -33,15 +33,15 @@ class HassBme280:
             self.humidity.set_value(current_humidity)
             
             if self.temperature.get_needs_update():
-                await self.hass.send_update(self.temperature.get_value(), "°C", "temperature", self.temperature_config['name'], self.temperature_config['entity'])
+                await self.hass.send_update(self.temperature.get_value(), "°C", "temperature", **self.temperature_config)
                 self.temperature.set_value_updated()
             
             if self.pressure.get_needs_update():
-                await self.hass.send_update(self.pressure.get_value(), "hPa", "atmospheric_pressure", self.pressure_config['name'], self.pressure_config['entity'])
+                await self.hass.send_update(self.pressure.get_value(), "hPa", "atmospheric_pressure", **self.pressure_config)
                 self.pressure.set_value_updated()
                 
             if self.humidity.get_needs_update():
-                await self.hass.send_update(self.humidity.get_value(), "%", "humidity", self.humidity_config['name'], self.humidity_config['entity'])
+                await self.hass.send_update(self.humidity.get_value(), "%", "humidity", **self.humidity_config)
                 self.humidity.set_value_updated()
             
             await asyncio.sleep(1)
---
+++ sensor/hassmotion.py
@@ -0,0 +1,34 @@
+import asyncio
+import machine
+
+class HassMotion:
+    def __init__(self, hass, pin, friendly_name, sensor):
+        self.hass = hass
+        self.pin = machine.Pin(pin, machine.Pin.IN)
+        self.pin.irq(self.interrupt, machine.Pin.IRQ_RISING)
+        self.friendly_name = friendly_name
+        self.sensor = sensor
+        self.tsf = asyncio.ThreadSafeFlag()
+
+    def interrupt(self, pin):
+        self.tsf.set()
+
+    CREATION_PRIORITY = 1
+    def create(provider):
+        config = provider['config']['motion']
+        return HassMotion(provider['hass.Hass'], config['pin'], config['friendly_name'], config['sensor'])
+    
+    async def start(self):
+        while True:
+            # Wait for initial motion signal
+            await self.tsf.wait()
+            await self.hass.send_update("on", None, "motion", self.friendly_name, self.sensor)
+            
+            # Once motion has been detected
+            try:
+                while True:
+                    # Wait for either another signal, or a timeout
+                    await asyncio.wait_for(self.tsf.wait(), 300)
+            except asyncio.TimeoutError:
+                # The timer elapsed without detection so signal no motion
+                await self.hass.send_update("off", None, "motion", self.friendly_name, self.sensor)
