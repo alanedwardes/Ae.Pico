163593264175b5d02982aa7dc7a53635b06bf799
Alan Edwardes <alan@alanedwardes.com>
2025-02-24 19:04:36 +0000

    Create picoscrollpack.py

---
+++ scrollclock/picoscrollpack.py
@@ -0,0 +1,11 @@
+from picographics import PicoGraphics, DISPLAY_SCROLL_PACK, PEN_P8
+from picoscroll import PicoScroll
+
+class PicoScrollPack:
+    def create(provider):
+        config = provider['config'].get('display', {})
+        provider['graphics'] = PicoGraphics(DISPLAY_SCROLL_PACK, pen_type=PEN_P8)
+        provider['scroll'] = PicoScroll()
+              
+    async def start(self):
+        raise NotImplementedError
