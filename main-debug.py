75cc4b803f64e6d704540313f21c39b0fb8e0076
Alan Edwardes <alan@alanedwardes.com>
2025-04-09 22:44:02 +0100

    Create main-debug.py

---
+++ main-debug.py
@@ -0,0 +1,30 @@
+import sys
+import config
+import asyncio
+import time
+from network import WLAN, STA_IF
+from servicefactory import ServiceFactory
+
+provider = {'nic': WLAN(STA_IF), 'config': config.config}
+
+def save_exception(exception):
+    with open('exceptions.log', 'a+') as f:
+        f.write(f'{time.time()}: ')
+        sys.print_exception(exception, f)
+
+def set_global_exception():
+    def handle_exception(loop, context):
+        save_exception(context["exception"])
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(handle_exception)
+set_global_exception()
+
+factory = ServiceFactory(provider)
+
+try:
+    asyncio.run(factory.run_components_forever())
+except Exception as e:
+    save_exception(e)
+    raise
+finally:
+    asyncio.new_event_loop()
