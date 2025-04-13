c03667700f0ae8b98c0c1fdf772ebb382ba47d25
Alan Edwardes <alan@alanedwardes.com>
2025-04-13 18:42:37 +0100

    Adds timeout for hassmotion.

--- sensor/hassmotion.py
+++ sensor/hassmotion.py
@@ -2,12 +2,13 @@ import asyncio
 import machine
 
 class HassMotion:
-    def __init__(self, hass, pin, friendly_name, sensor):
+    def __init__(self, hass, pin, friendly_name, sensor, timeout_seconds):
         self.hass = hass
         self.pin = machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_DOWN)
         self.pin.irq(self.interrupt, machine.Pin.IRQ_RISING)
         self.friendly_name = friendly_name
         self.sensor = sensor
+        self.timeout_seconds = timeout_seconds
         self.tsf = asyncio.ThreadSafeFlag()
 
     def interrupt(self, pin):
@@ -16,7 +17,7 @@ class HassMotion:
     CREATION_PRIORITY = 1
     def create(provider):
         config = provider['config']['motion']
-        return HassMotion(provider['hass.Hass'], config['pin'], config['friendly_name'], config['sensor'])
+        return HassMotion(provider['hass.Hass'], config['pin'], config['friendly_name'], config['sensor'], config.get('timeout_seconds', 300))
     
     async def start(self):
         while True:
@@ -28,7 +29,7 @@ class HassMotion:
             try:
                 while True:
                     # Wait for either another signal, or a timeout
-                    await asyncio.wait_for(self.tsf.wait(), 300)
+                    await asyncio.wait_for(self.tsf.wait(), self.timeout_seconds)
             except asyncio.TimeoutError:
                 # The timer elapsed without detection so signal no motion
                 await self.hass.send_update("off", None, "motion", self.friendly_name, self.sensor)
