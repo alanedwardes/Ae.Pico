871c293d70d7951c1b167f1affbddfa833feb81c
Alan Edwardes <alan@alanedwardes.com>
2025-04-26 21:27:59 +0100

    Implements debounce for hassmotion, with a default of 100ms.

--- sensor/hassmotion.py
+++ sensor/hassmotion.py
@@ -2,13 +2,14 @@ import asyncio
 import machine
 
 class HassMotion:
-    def __init__(self, hass, pin, friendly_name, sensor, timeout_seconds):
+    def __init__(self, hass, pin, friendly_name, sensor, timeout_seconds, debounce_ms):
         self.hass = hass
         self.pin = machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_DOWN)
         self.pin.irq(self.interrupt)
         self.friendly_name = friendly_name
         self.sensor = sensor
         self.timeout_seconds = timeout_seconds
+        self.debounce_ms = debounce_ms
         self.tsf = asyncio.ThreadSafeFlag()
 
     def interrupt(self, pin):
@@ -17,11 +18,12 @@ class HassMotion:
     CREATION_PRIORITY = 1
     def create(provider):
         config = provider['config']['motion']
-        return HassMotion(provider['hass.Hass'], config['pin'], config['friendly_name'], config['sensor'], config.get('timeout_seconds', 300))
+        return HassMotion(provider['hass.Hass'], config['pin'], config['friendly_name'], config['sensor'], config.get('timeout_seconds', 300), config.get('debounce_ms', 100))
     
     async def start(self):
         while True:
             await self.tsf.wait()
+            await asyncio.sleep_ms(self.debounce_ms)
             
             if self.pin.value() == 1:
                 await self.hass.send_update("on", None, "motion", self.friendly_name, self.sensor)
