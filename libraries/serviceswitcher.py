95422305344a54e3317cfe63df2a2dcdcbb83aeb
Alan Edwardes <alan@alanedwardes.com>
2025-05-11 21:46:06 +0100

    Adds ServiceSwitcher, a service to loop a list of services in sequence and call an activation method with True/False depending on whether active or not.

---
+++ libraries/serviceswitcher.py
@@ -0,0 +1,18 @@
+import asyncio
+
+class ServiceSwitcher:
+    def __init__(self, provider, services, time_ms):
+        self.provider = provider
+        self.services = services
+        self.time_ms = time_ms
+    
+    def create(provider):
+        config = provider['config']['switcher']
+        return ServiceSwitcher(provider, config['services'], config['time_ms'])
+                 
+    async def start(self):
+        while True:
+            for service in self.services:
+                self.provider[service].activate(True)
+                await asyncio.sleep_ms(self.time_ms)
+                self.provider[service].activate(False)
