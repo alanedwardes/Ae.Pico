3cf06a29b4241b8dc103d25f113626c2ad3a1884
Alan Edwardes <alan@alanedwardes.com>
2025-06-18 23:01:11 +0100

    Re-writes temperature gauge logic.

--- infodisplay/temperaturedisplay.py
+++ infodisplay/temperaturedisplay.py
@@ -24,6 +24,9 @@ def get_mapped_range_value_clamped(input_range, output_range, value):
 def get_mapped_range_value_unclamped(input_range, output_range, value):
     return get_range_value(output_range, get_range_pct(input_range, value))
 
+def point_on_circle(x, y, radius, angle):
+    return (x + radius * math.cos(angle), y + radius * math.sin(angle));
+
 class TemperatureDisplay:
     def __init__(self, display, hass, entity_ids):
         self.display = display
@@ -65,6 +68,77 @@ class TemperatureDisplay:
     async def start(self):
         await self.hass.subscribe(self.entity_ids.values(), self.entity_updated)
         await asyncio.Event().wait()
+        
+    def circle(self, x, y, radius):
+        return self.display.circle(int(x), int(y), int(radius))
+    
+    def polygon(self, points):
+        return self.display.polygon([(int(point[0]), int(point[1])) for point in points])
+        
+    def draw_gauge(self, position, size, minimum_temperature, maximum_temperature, current_temperature):
+        centre = [size[0] / 2 + position[0], size[1] / 2 + position[1]]
+
+        guage_radius = size[1] * 0.45
+        guage_thickness = size[1] * 0.05
+        
+        self.display.set_pen(self.display.create_pen(128, 128, 128))
+
+        # Outer gauge
+        self.circle(centre[0], centre[1], guage_radius + guage_thickness)
+        
+        self.display.set_pen(self.display.create_pen(0, 0, 0))
+
+        # Inner gauge
+        self.circle(centre[0], centre[1], guage_radius - guage_thickness)
+
+        extent_x = [centre[0] - size[1] * 0.5, centre[0] + size[1] * 0.5]
+
+        # Polygon base
+        self.polygon([
+            [extent_x[0], position[1] + size[1]],
+            [extent_x[0], position[1] + size[1] * 0.8],
+            [centre[0], centre[1]],
+            [extent_x[1], position[1] + size[1] * 0.8],
+            [extent_x[1], position[1] + size[1]]
+        ])
+
+        degrees_offset = 65
+        gauge_min_max_radians = [
+            math.radians(90 + degrees_offset),
+            math.radians(90 + 360 - degrees_offset)
+        ]
+        
+        self.display.set_pen(self.display.create_pen(128, 128, 128))
+
+        # Rounded cap start
+        rounded_cap_start = point_on_circle(centre[0], centre[1], guage_radius, gauge_min_max_radians[0] - 0.1)
+        self.circle(rounded_cap_start[0], rounded_cap_start[1], guage_thickness)
+
+        # Rounded cap end
+        rounded_cap_end = point_on_circle(centre[0], centre[1], guage_radius, gauge_min_max_radians[1] + 0.1)
+        self.circle(rounded_cap_end[0], rounded_cap_end[1], guage_thickness)
+
+        radians = get_mapped_range_value_clamped(
+            [minimum_temperature, maximum_temperature],
+            gauge_min_max_radians,
+            current_temperature
+        )
+        
+        notch_point = point_on_circle(centre[0], centre[1], guage_radius, radians)
+        
+        self.display.set_pen(self.display.create_pen(0, 0, 0))
+        self.circle(notch_point[0], notch_point[1], guage_thickness * 2)
+        
+        self.display.set_pen(self.display.create_pen(255, 255, 255))
+        self.circle(notch_point[0], notch_point[1], guage_thickness)
+        
+        self.display.set_font("sans")
+        self.draw_text(f"{current_temperature:.0f}", 1, position[0], position[1], size[0], size[1])
+        
+        self.display.set_font("bitmap8")
+        text_y = int(position[1] + size[1] * 0.75)
+        self.draw_text(f"{minimum_temperature:.0f}", 2, position[0], text_y, size[0] * 0.5, size[1] * 0.1)
+        self.draw_text(f"{maximum_temperature:.0f}", 2, position[0] + size[0] * 0.5, text_y, size[0] * 0.5, size[1] * 0.1)
 
     def update(self):
         default_entity = dict(s = '0')
@@ -72,45 +146,8 @@ class TemperatureDisplay:
         maximum_temperature = float(self.entities.get(self.entity_ids['maximum_temp_entity_id'], default_entity)['s'])
         current_temperature = float(self.entities.get(self.entity_ids['current_temp_entity_id'], default_entity)['s'])
         
-        start_x = self.display_width - 64
-        
-        width = 64
-        
-        height = 70
-        
-        self.display.set_font("sans")
         self.display.set_pen(self.display.create_pen(0, 0, 0))
-        self.display.rectangle(start_x, 0, width, height)
+        self.display.rectangle(self.display_width - 64, 0, 64, 70)
         
-        self.display.set_pen(self.display.create_pen(255, 255, 255))
-        
-        circle_center_x = start_x + int(width / 2.0)
-        circle_center_y = 30
-        
-        self.display.set_pen(self.display.create_pen(255, 255, 255))
-        self.display.circle(circle_center_x, circle_center_y, 30)
-        
-        self.display.set_pen(self.display.create_pen(0, 0, 0))        
-        self.display.circle(circle_center_x, circle_center_y, 25)
-        
-        self.display.triangle(start_x, 50, start_x + int(width / 2.0), int(height / 2.0), self.display_width, 50)
-        self.display.rectangle(start_x, 50, width, 20)
-        
-        guage_min_max_radians = (2.6, 6.8)        
-        radians = get_mapped_range_value_clamped((minimum_temperature, maximum_temperature), guage_min_max_radians, current_temperature)
-        
-        guage_radius = 27.5        
-        px = circle_center_x + guage_radius * math.cos(radians);
-        py = circle_center_y + guage_radius * math.sin(radians);
-        self.display.circle(int(px), int(py), 6)
-        
-        self.display.set_pen(self.display.create_pen(255, 255, 255))
-        
-        self.display.circle(int(px), int(py), 3)
-        self.draw_text(f"{current_temperature:.0f}", 1, start_x, 12, width, int(height / 2.0))
-        
-        self.display.set_font("bitmap8")
-        self.display.text(f"{minimum_temperature:.0f}", start_x + 10, 52, scale=2)
-        self.display.text(f"{maximum_temperature:.0f}", start_x + 40, 52, scale=2)
-        
+        self.draw_gauge((self.display_width - 64, 0), (64, 64), minimum_temperature, maximum_temperature, current_temperature)
         self.display.update()
