c70c8105ee7a12e839d80ffb791a114a59922397
Alan Edwardes <alan@alanedwardes.com>
2025-06-26 00:14:57 +0100

    Improves upon UV vis.

--- infodisplay/uvdisplay.py
+++ infodisplay/uvdisplay.py
@@ -53,21 +53,7 @@ class UvDisplay:
         self.__update()
         update_time_ms = utime.ticks_diff(utime.ticks_ms(), start_update_ms)
         print(f"RainDisplay: {update_time_ms}ms")
-
-    def set_pen_for_uv(self, uv):
-        if uv < 1:
-            self.display.set_pen(self.display.create_pen(96, 96, 96))
-        elif uv < 3:
-            self.display.set_pen(self.display.create_pen(113, 180, 102))
-        elif uv < 6:
-            self.display.set_pen(self.display.create_pen(248, 231, 28))
-        elif uv < 8:
-            self.display.set_pen(self.display.create_pen(255, 149, 12))
-        elif uv < 11:
-            self.display.set_pen(self.display.create_pen(215, 41, 33))
-        else:
-            self.display.set_pen(self.display.create_pen(102, 0, 224))
-    
+   
     def __update(self):
         hours = self.hass.entities.get(self.entity_id, {}).get('a', {}).get('hours', [])
         
@@ -113,7 +99,7 @@ class UvDisplay:
             
             max_column_height = 70
             
-            self.set_pen_for_uv(uv)
+            self.display.set_pen(self.display.create_pen(*colors.get_color_for_uv(uv)))
 
             self.draw_text(f"{uv}", sx, sy + max_column_height + 5, column_width, scale=2)
             
@@ -128,19 +114,29 @@ class UvDisplay:
         self.display.set_pen(self.display.create_pen(64, 64, 64))
         self.display.rectangle(0, chart_y + chart_height, self.display_width, 2)
 
-        polygon = []
-        polygon.append((0, chart_y + chart_height))
-
-        for px, py in chart.draw_chart(0, chart_y, self.display_width, chart_height, [hour['u'] / 12 for hour in hours], 32):
-            polygon.append((int(px), int(py)))
-
-        polygon.append((self.display_width, chart_y + chart_height))
-
-        self.display.set_pen(self.display.create_pen(128, 64, 0))
-        self.display.polygon(polygon)
-
         self.display.set_pen(self.display.create_pen(255, 128, 0))
         for px, py in chart.draw_chart(0, chart_y, self.display_width, chart_height, [hour['u'] / 12 for hour in hours]):
+            uv = hours[min(10, int(px / (self.display_width / len(hours))))]['u']
+            self.display.set_pen(self.display.create_pen(*colors.get_color_for_uv(uv)))
             self.display.circle(int(px), int(py), 2)
+
+        self.display.set_pen(self.display.create_pen(*colors.get_color_for_uv(max(hour['u'] for hour in hours))))
+        self.display.rectangle(3, y_start + 40, 64, 16)
+
+        # After drawing the chart and before self.display.update()
+        max_uv = max(hour['u'] for hour in hours)
+        if max_uv <= 2:
+            uv_word = "LOW"
+        elif max_uv <= 5:
+            uv_word = "MODERATE"
+        elif max_uv <= 7:
+            uv_word = "HIGH"
+        elif max_uv <= 10:
+            uv_word = "VERY HIGH"
+        else:
+            uv_word = "EXTREME"
+
+        self.display.set_pen(self.display.create_pen(0, 0, 0))
+        self.draw_text(uv_word, 3, y_start + 40, 64, 1)
 
         self.display.update()
