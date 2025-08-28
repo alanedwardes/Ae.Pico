a3beedd61e5c362c097806cf0cd24a5f518ba83c
Alan Edwardes <alan@alanedwardes.com>
2025-08-28 18:06:12 +0100

    Improve error handling for cpython entry point.

--- cpython/main.py
+++ cpython/main.py
@@ -4,11 +4,15 @@ sys.path.insert(1, '../infodisplay')
 
 import config
 import asyncio
+import traceback
 from servicefactory import ServiceFactory
 
 provider = {'config': config.config}
 
-factory = ServiceFactory(provider)
+def handle_factory_exception(e):
+    traceback.print_exception(type(e), e, e.__traceback__)
+
+factory = ServiceFactory(provider, handle_factory_exception)
 
 try:
     asyncio.run(factory.run_components_forever())
