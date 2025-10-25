cbe8e16de599b4e6bb8c01337c2a7c7bea35cdef
Alan Edwardes <alan@alanedwardes.com>
2025-10-25 12:54:34 +0100

    Fix race in hassgeiger.

--- sensor/hassgeiger.py
+++ sensor/hassgeiger.py
@@ -44,7 +44,8 @@ class HassGeiger:
         while True:
             await asyncio.sleep_ms(self.update_time_ms)
             
-            cpm = self.click_tracker.get_clicks_per_minute()
-            await self.hass.send_update(cpm / self.cpm_ratio, "μSv/h", None, self.friendly_name, self.sensor)
-            await self.hass.send_update(cpm, "CPM", None, self.friendly_name + " CPM", self.sensor + "_cpm")
-            self.click_tracker = ClickTracker()
+			old_tracker = self.click_tracker
+			self.click_tracker = ClickTracker()
+			cpm = old_tracker.get_clicks_per_minute()
+			await self.hass.send_update(cpm / self.cpm_ratio, "μSv/h", None, self.friendly_name, self.sensor)
+			await self.hass.send_update(cpm, "CPM", None, self.friendly_name + " CPM", self.sensor + "_cpm")
