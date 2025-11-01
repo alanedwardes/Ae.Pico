4caaefd83cea1e56ad37ebeb8ccc6b57ff49e048
Alan Edwardes <alan@alanedwardes.com>
2025-11-01 23:51:01 +0000

    Timedisplay now only updates the components which have changed.

--- infodisplay/timedisplay.py
+++ infodisplay/timedisplay.py
@@ -16,6 +16,11 @@ class TimeDisplay:
         
         self.last_update_time_ms = 0
         
+        # Cache last-rendered strings to avoid unnecessary redraws
+        self._last_time_text = None   # HH:MM
+        self._last_day_text = None    # Day-of-week
+        self._last_sec_text = None    # SS
+        
     
     CREATION_PRIORITY = 1
     def create(provider):
@@ -39,21 +44,43 @@ class TimeDisplay:
         self.last_update_time_ms = utime.ticks_diff(utime.ticks_ms(), start_update_ms)
         print(f"TimeDisplay: {self.last_update_time_ms}ms")
 
-    def __update(self):       
+    def __update(self):
+        # Layout constants
         height = 70
-        width = self.display_width - 64
-        
-        self.display.rect(0, 0, width, height, 0x0000, True)
+        section_height = height // 2
+        time_width = 200
+        date_seconds_width = self.display_width - time_width - 64  # the temp display is 64px
 
         now = self.rtc.datetime()
-                
-        section_height = height / 2
-        
-        time_width = 200
-        textbox.draw_textbox(self.display, '%02i:%02i' % (now[4], now[5]), 0, 0, time_width, height, color=0xFFFF, font='headline', scale=1)
 
-        date_seconds_width = self.display_width - time_width - 64 # the temp display is 64px        
-        textbox.draw_textbox(self.display, f'{self.DAYS[now[3]-1]}', time_width, 0, date_seconds_width, section_height, color=0xFFFF, font='regular', scale=1)
-        textbox.draw_textbox(self.display, '%02i' % now[6], time_width, section_height, date_seconds_width, section_height, color=0xFFFF, font='regular', scale=1)
-        
-        self.display.update()
+        # Compute current texts
+        time_text = '%02i:%02i' % (now[4], now[5])
+        day_text = f'{self.DAYS[now[3]-1]}'
+        sec_text = '%02i' % now[6]
+
+        any_drawn = False
+
+        # Redraw HH:MM only when it changes
+        if time_text != self._last_time_text:
+            # Clear time area then draw
+            self.display.rect(0, 0, time_width, height, 0x0000, True)
+            textbox.draw_textbox(self.display, time_text, 0, 0, time_width, height, color=0xFFFF, font='headline', scale=1)
+            self._last_time_text = time_text
+            any_drawn = True
+
+        # Redraw day-of-week only when it changes
+        if day_text != self._last_day_text:
+            self.display.rect(time_width, 0, date_seconds_width, section_height, 0x0000, True)
+            textbox.draw_textbox(self.display, day_text, time_width, 0, date_seconds_width, section_height, color=0xFFFF, font='regular', scale=1)
+            self._last_day_text = day_text
+            any_drawn = True
+
+        # Redraw seconds each time they change
+        if sec_text != self._last_sec_text:
+            self.display.rect(time_width, section_height, date_seconds_width, section_height, 0x0000, True)
+            textbox.draw_textbox(self.display, sec_text, time_width, section_height, date_seconds_width, section_height, color=0xFFFF, font='regular', scale=1)
+            self._last_sec_text = sec_text
+            any_drawn = True
+
+        if any_drawn:
+            self.display.update()
