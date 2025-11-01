4e9a38c5b9dc7d4706dd6295a2ada2e7480e9991
Alan Edwardes <alan@alanedwardes.com>
2025-11-01 23:55:59 +0000

    More work on newsdisplay.

--- infodisplay/newsdisplay.py
+++ infodisplay/newsdisplay.py
@@ -43,32 +43,24 @@ class NewsDisplay:
         
         stories = self.get_stories()
             
-        self.white = 0xFFFF
-        self.black = 0x0000
-        self.highlight = 0xF346
-        self.grey = 0x8410
-        
         y_offset = 70
         
-        self.display.rect(0, y_offset, self.display_width, self.display_height - y_offset, self.black, True)
+        self.display.rect(0, y_offset, self.display_width, self.display_height - y_offset, 0x0000, True)
         
         try:
             story = stories[self.story_index]
         except IndexError:
             story = dict(t='?', p='?')
 
-        label_height = 25
-            
+        label_height = 24
+
+        self.display.rect(0, y_offset, self.display_width, label_height, 0xb000, True)
         textbox.draw_textbox(self.display, "%i/%i %s" % (self.story_index + 1, len(stories), story['p']), 
-                            0, y_offset, self.display_width, label_height, color=self.grey, font='small', align='center')
+                            0, y_offset, self.display_width, label_height, color=0xFFFF, font='small')
         
         y_offset += label_height
-        # Red divider between metadata and story
-        divider_height = 4
-        self.display.rect(0, y_offset, self.display_width, divider_height, 0xb000, True)
-        y_offset += divider_height
         
-        textbox.draw_textbox(self.display, story['t'], 0, y_offset, self.display_width, self.display_height - y_offset, color=self.white, font='regular', wrap=True)
+        textbox.draw_textbox(self.display, story['t'], 0, y_offset, self.display_width, self.display_height - y_offset, color=0xFFFF, font='regular', wrap=True)
         self.display.update()
         update_time_ms = utime.ticks_diff(utime.ticks_ms(), start_update_ms)
         print(f"NewsDisplay: {update_time_ms}ms")
