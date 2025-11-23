b647a563c66f94f9f8f705fd469f4fffff8bb92b
Alan Edwardes <alan@alanedwardes.com>
2025-11-23 17:23:04 +0000

    Improve traindisplay.py

--- infodisplay/traindisplay.py
+++ infodisplay/traindisplay.py
@@ -20,7 +20,7 @@ import textbox
 from httpstream import parse_url
 
 # Define Departure named tuple for memory efficiency
-Departure = namedtuple('Departure', 'scheduled_time expected_time destination train_class cancelled delayed')
+Departure = namedtuple('Departure', 'scheduled_time expected_time destination platform train_class cancelled delayed')
 
 def format_time_to_hhmm(time_str):
     """Convert datetime string to HH:MM format."""
@@ -148,25 +148,48 @@ class TrainDisplay:
         update_time_ms = utime.ticks_diff(utime.ticks_ms(), start_update_ms)
         print(f"TrainDisplay: {update_time_ms}ms")
         
+    def __draw_header_row(self, y_offset):
+        """Draw header row with column labels."""
+        header_color = 0x8410  # Grey for header
+        
+        # Define column widths (same as departure rows)
+        time_width = 50
+        destination_width = 160
+        platform_width = 30
+        train_class_width = 30
+        expected_width = 50
+        
+        # Draw header labels
+        textbox.draw_textbox(self.display, 'Time', 0, y_offset, time_width, 20, color=header_color, font='small')
+        textbox.draw_textbox(self.display, 'Destination', time_width, y_offset, destination_width, 20, color=header_color, font='small', align='left')
+        textbox.draw_textbox(self.display, 'Plt', time_width + destination_width, y_offset, platform_width, 20, color=header_color, font='small')
+        textbox.draw_textbox(self.display, 'Cls', time_width + destination_width + platform_width, y_offset, train_class_width, 20, color=header_color, font='small')
+        textbox.draw_textbox(self.display, 'Exp', time_width + destination_width + platform_width + train_class_width, y_offset, expected_width, 20, color=header_color, font='small')
+        
+        return 19
+    
     def __draw_departure_row(self, departure, y_offset):
         destination = departure.destination or ''
         scheduled = departure.scheduled_time or 'TBC'
         expected = departure.expected_time or 'TBC'
+        platform = departure.platform or '-'
         train_class = departure.train_class or '-'
         
         row_pen = get_color_for_train_status(scheduled, expected, departure.cancelled)
         
         # Define column widths and positions
         time_width = 50
-        destination_width = 190
+        destination_width = 160
+        platform_width = 30
         train_class_width = 30
         expected_width = 50
         
         # Draw each column using textbox
         textbox.draw_textbox(self.display, scheduled, 0, y_offset, time_width, 20, color=row_pen, font='small')
         textbox.draw_textbox(self.display, destination, time_width, y_offset, destination_width, 20, color=row_pen, font='small', align='left')
-        textbox.draw_textbox(self.display, train_class, time_width + destination_width, y_offset, train_class_width, 20, color=row_pen, font='small')
-        textbox.draw_textbox(self.display, expected, time_width + destination_width + train_class_width, y_offset, expected_width, 20, color=row_pen, font='small')
+        textbox.draw_textbox(self.display, platform, time_width + destination_width, y_offset, platform_width, 20, color=row_pen, font='small')
+        textbox.draw_textbox(self.display, train_class, time_width + destination_width + platform_width, y_offset, train_class_width, 20, color=row_pen, font='small')
+        textbox.draw_textbox(self.display, expected, time_width + destination_width + platform_width + train_class_width, y_offset, expected_width, 20, color=row_pen, font='small')
         
         return 19
     
@@ -208,14 +231,15 @@ class TrainDisplay:
             raw_data = ujson.loads(content.decode('utf-8'))
             
             # Convert flat array to structured data
-            # Format: [std, station, class, atd, std, station, class, atd, ...]
+            # Format: [std, station, platform, class, atd, std, station, platform, class, atd, ...]
             self.departures = []
-            for i in range(0, len(raw_data), 4):
-                if i + 3 < len(raw_data):
+            for i in range(0, len(raw_data), 5):
+                if i + 4 < len(raw_data):
                     std = raw_data[i]
                     station = raw_data[i + 1]
-                    train_class = raw_data[i + 2]
-                    atd = raw_data[i + 3]
+                    platform = raw_data[i + 2]
+                    train_class = raw_data[i + 3]
+                    atd = raw_data[i + 4]
                     
                     # Determine if cancelled or delayed
                     cancelled = False
@@ -233,12 +257,14 @@ class TrainDisplay:
                     scheduled_time_display = std_formatted if std_formatted else ''
                     expected_time_display = atd_formatted if atd_formatted else scheduled_time_display
                     destination_display = station if station else ''
+                    platform_display = platform if platform else ''
                     train_class_display = train_class if train_class else ''
                     
                     self.departures.append(Departure(
                         scheduled_time=scheduled_time_display,
                         expected_time=expected_time_display,
                         destination=destination_display,
+                        platform=platform_display,
                         train_class=train_class_display,
                         cancelled=cancelled,
                         delayed=delayed
@@ -260,6 +286,10 @@ class TrainDisplay:
         
         self.display.rect(0, y_offset, self.display_width, self.display_height - y_offset, 0x0000, True)
         
+        # Draw header row
+        y_offset += self.__draw_header_row(y_offset)
+        
+        # Draw departure rows
         for row in range(0, len(departures)):
             y_offset += self.__draw_departure_row(departures[row], y_offset)
             
