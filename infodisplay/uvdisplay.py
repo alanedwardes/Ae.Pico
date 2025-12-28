9396da9c7bb3a3057b8cc5bda79086df0608af55
Alan Edwardes <alan@alanedwardes.com>
2025-12-28 22:23:22 +0000

    Always draw UV values.

--- infodisplay/uvdisplay.py
+++ infodisplay/uvdisplay.py
@@ -31,11 +31,7 @@ class UvDisplay:
     def should_activate(self):
         if not self.uv_data:
             return False
-        
-        # Check if all UV values are 2 or below
-        if all(uv <= 2 for uv in self.uv_data):
-            return False
-        
+               
         first_idx = None
         last_idx = None
         for i, v in enumerate(self.uv_data):
