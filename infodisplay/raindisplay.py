cb3c925e44c93f9d564292d1179ee548423a95c4
Alan Edwardes <alan@alanedwardes.com>
2026-01-04 14:36:55 +0000

    Remove logging from raindisplay.

--- infodisplay/raindisplay.py
+++ infodisplay/raindisplay.py
@@ -187,12 +187,7 @@ class RainDisplay:
         if self.is_active == False:
             return
 
-        start_update_ms = utime.ticks_ms()
-        mem_before = gc.mem_alloc()
         await self.__update()
-        update_time_ms = utime.ticks_diff(utime.ticks_ms(), start_update_ms)
-        mem_after = gc.mem_alloc()
-        print(f"RainDisplay: {update_time_ms}ms, mem: {mem_before} -> {mem_after} ({mem_after - mem_before:+d})")
 
     async def __update(self):
         if len(self.weather_data) == 0:
