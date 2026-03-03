36d3641debdbf31e998991e568c09773e4771de2
Alan Edwardes <alan@alanedwardes.com>
2026-03-03 22:02:52 +0000

    Remove redundant code.

--- infodisplay/ili9488display.py
+++ infodisplay/ili9488display.py
@@ -1,6 +1,6 @@
 import asyncio
 from machine import SPI, Pin
-from ili9488 import ILI9488, REFLECT
+from ili9488 import ILI9488, REFLECT, PORTRAIT, USD
 from drawing import Drawing
 
 # Default display dimensions
@@ -33,6 +33,10 @@ class ILI9488Display:
 
         mode = config.get('mode', 'RGB565')
 
+        rotation = int(config.get('rotate', 0))
+        rotation_modes = {0: REFLECT, 90: PORTRAIT | REFLECT | USD, 180: REFLECT | USD, 270: PORTRAIT | REFLECT}
+        disp_mode = rotation_modes.get(rotation, REFLECT)
+
         ili = ILI9488(
             spi,
             cs=cs,
@@ -41,15 +45,11 @@ class ILI9488Display:
             backlight=backlight,
             height=display_height,
             width=display_width,
-            disp_mode=REFLECT,
+            disp_mode=disp_mode,
             display=(0, 0, 1, True, True),
             scale=scale,
             source_color_mode=mode,
         )
-
-        # Optional rotation from config
-        rotation = config.get('rotate', 0)  # degrees: 0/90/180/270
-        ili.set_rotation_degrees(int(rotation))
 
         # Drawing uses reduced framebuffer dimensions
         drawing = Drawing(fb_width, fb_height, color_mode=mode)
