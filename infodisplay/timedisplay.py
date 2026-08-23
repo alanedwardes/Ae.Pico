68963f9d475c1f64d96796e26b1e11fa15b3da25
Alan Edwardes <alan@alanedwardes.com>
2026-08-24 00:25:26 +0100

    Better timedisplay optimisations.

--- infodisplay/timedisplay.py
+++ infodisplay/timedisplay.py
@@ -1,6 +1,8 @@
 import asyncio
 import gc
+import math
 import textbox
+from bmfont import draw_text, measure_text
 
 class TimeDisplay:
     MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
@@ -52,6 +54,42 @@ class TimeDisplay:
         self._cal_region = (self.time_width, 0, self.date_seconds_width, self.section_height)
         self._sec_region = (self.sec_x, self.section_height, self.sec_width, self.section_height)
         self._ms_region = (self.ms_x, self.section_height, self.ms_width, self.section_height)
+
+        self._sec_font, self._sec_pages = textbox.get_font('regular')
+        self._ms_font, self._ms_pages = textbox.get_font('small')
+        self._linebuf = self.display.get_scratch_buffer(max(self._sec_font.scale_w, self._ms_font.scale_w))
+
+        s = max(0.000001, float(self.font_scale))
+        if s < 1.0:
+            self._scale_up = 1
+            self._scale_down = max(1, int(round(1.0 / s)))
+        else:
+            self._scale_up = max(1, int(round(s)))
+            self._scale_down = 1
+
+        sec_align_fraction = 0.5 * (1 - self.show_milliseconds)
+        self._sec_origin_x = self._precompute_origin_x(self._padded_numbers, self._sec_font, self.sec_x, self.sec_width, sec_align_fraction)
+        self._sec_origin_y = self._precompute_origin_y(self._sec_font, self.section_height, self.section_height)
+
+        self._ms_origin_x = self._precompute_origin_x(self._tenth_numbers, self._ms_font, self.ms_x, self.ms_width, 0.0)
+        self._ms_origin_y = self._precompute_origin_y(self._ms_font, self.section_height, self.section_height)
+
+        self._sec_clip = (int(self.sec_x), int(self.section_height), int(self.sec_width), int(self.section_height))
+        self._ms_clip = (int(self.ms_x), int(self.section_height), int(self.ms_width), int(self.section_height))
+
+    def _precompute_origin_x(self, strings, font, x, width, align_fraction):
+        scale_up, scale_down = self._scale_up, self._scale_down
+        origins = []
+        for text in strings:
+            bounds_w, _h, min_x, _min_y = measure_text(font, text)
+            text_width = (bounds_w * scale_up) // scale_down
+            text_x = x + (width - text_width) * align_fraction
+            origins.append(math.floor(text_x - (min_x * scale_up) // scale_down))
+        return origins
+
+    def _precompute_origin_y(self, font, y, height):
+        text_height = (font.line_height * self._scale_up) // self._scale_down
+        return math.floor(y + (height - text_height) * 0.5)
 
     CREATION_PRIORITY = 2
     def create(provider):
@@ -121,22 +159,23 @@ class TimeDisplay:
         # 3. Seconds Display
         if now[5] != self._last_second:
             self._last_second = now[5]
-            # Use pre-allocated string
-            if now[5] < 60:
-                sec_text = self._padded_numbers[now[5]]
-            else:
-                sec_text = "00" # Safety fallback
+            sec_idx = now[5] if now[5] < 60 else 0
 
-            sec_align = 'left' if self.show_milliseconds else 'center'
-            await textbox.draw_textbox(self.display, sec_text, self.sec_x, section_height, self.sec_width, section_height, color=0xFFFFFF, font='regular', scale=font_scale, align=sec_align, background=0x000000)
+            draw_text(self.display, self.display_width, self.display_height, self._sec_font, self._sec_pages,
+                      self._padded_numbers[sec_idx], self._sec_origin_x[sec_idx], self._sec_origin_y,
+                      kerning=True, scale_up=self._scale_up, scale_down=self._scale_down, color=0xFFFFFF,
+                      linebuf=self._linebuf, clip=self._sec_clip, background=0x000000, top_edge=True, bottom_edge=True)
             self.display.update(self._sec_region)
 
         # 4. Milliseconds (Tenths) Display
         if not self.show_milliseconds:
             return
-        tenth = (now[8] // 100) % 10 # Ensure 0-9 range
+        tenth = (now[8] // 100) % 10
         if tenth != self._last_tenth:
             self._last_tenth = tenth
 
-            await textbox.draw_textbox(self.display, self._tenth_numbers[tenth], self.ms_x, section_height, self.ms_width, section_height, color=0xFFFFFF, font='small', scale=font_scale, align='left', background=0x000000)
+            draw_text(self.display, self.display_width, self.display_height, self._ms_font, self._ms_pages,
+                      self._tenth_numbers[tenth], self._ms_origin_x[tenth], self._ms_origin_y,
+                      kerning=True, scale_up=self._scale_up, scale_down=self._scale_down, color=0xFFFFFF,
+                      linebuf=self._linebuf, clip=self._ms_clip, background=0x000000, top_edge=True, bottom_edge=True)
             self.display.update(self._ms_region)
