28d331a93340580c530ad345e549187d82468c1f
Alan Edwardes <alan@alanedwardes.com>
2026-08-26 09:18:37 +0100

    Make ili9488display driver async.

---
+++ infodisplay/asyncspidriver.py
@@ -0,0 +1,161 @@
+import time
+import _thread
+import array
+import uctypes
+import micropython
+from mipidcs import _rgb565_to_888_line, _rgb332_to_888_line
+
+
+@micropython.viper
+def core1_spi_push565_888(dma_read_addr: int, dma_write_addr: int,
+                          dma_count_addr: int, dma_ctrl_addr: int,
+                          ctrl_word: int, spi_dr: int, spi_sr: int,
+                          write_len: int, linebuf_a: int, linebuf_b: int,
+                          fb: ptr16, fb_width: int, fb_ptr0: int,
+                          rw: int, rh: int, lut: ptr8):
+    r_read = ptr32(dma_read_addr)
+    r_write = ptr32(dma_write_addr)
+    r_count = ptr32(dma_count_addr)
+    r_ctrl = ptr32(dma_ctrl_addr)
+    sr = ptr32(spi_sr)
+    r_write[0] = spi_dr
+    cur = linebuf_a
+    nxt = linebuf_b
+    _rgb565_to_888_line(ptr8(cur), fb, fb_ptr0, rw, lut)
+    r_read[0] = cur
+    r_count[0] = write_len
+    r_ctrl[0] = ctrl_word
+    i = 1
+    while i < rh:
+        _rgb565_to_888_line(ptr8(nxt), fb, fb_ptr0 + i * fb_width, rw, lut)
+        while r_ctrl[0] & 0x4000000:
+            pass
+        r_read[0] = nxt
+        r_count[0] = write_len
+        r_ctrl[0] = ctrl_word
+        t = cur; cur = nxt; nxt = t
+        i += 1
+    while r_ctrl[0] & 0x4000000:
+        pass
+    while sr[0] & 0x10:
+        pass
+
+
+@micropython.viper
+def core1_spi_push332_888(dma_read_addr: int, dma_write_addr: int,
+                          dma_count_addr: int, dma_ctrl_addr: int,
+                          ctrl_word: int, spi_dr: int, spi_sr: int,
+                          write_len: int, linebuf_a: int, linebuf_b: int,
+                          fb: ptr8, fb_width: int, fb_ptr0: int,
+                          rw: int, rh: int, lut: ptr8):
+    r_read = ptr32(dma_read_addr)
+    r_write = ptr32(dma_write_addr)
+    r_count = ptr32(dma_count_addr)
+    r_ctrl = ptr32(dma_ctrl_addr)
+    sr = ptr32(spi_sr)
+    r_write[0] = spi_dr
+    cur = linebuf_a
+    nxt = linebuf_b
+    _rgb332_to_888_line(ptr8(cur), fb, fb_ptr0, rw, lut)
+    r_read[0] = cur
+    r_count[0] = write_len
+    r_ctrl[0] = ctrl_word
+    i = 1
+    while i < rh:
+        _rgb332_to_888_line(ptr8(nxt), fb, fb_ptr0 + i * fb_width, rw, lut)
+        while r_ctrl[0] & 0x4000000:
+            pass
+        r_read[0] = nxt
+        r_count[0] = write_len
+        r_ctrl[0] = ctrl_word
+        t = cur; cur = nxt; nxt = t
+        i += 1
+    while r_ctrl[0] & 0x4000000:
+        pass
+    while sr[0] & 0x10:
+        pass
+
+
+class AsyncSpiDriver:
+    def __init__(self, display, framebuffer, fb_width, fb_height):
+        self.display = display
+        self._fb = framebuffer
+        self.fb_width = fb_width
+        self.fb_height = fb_height
+        self._frames = array.array('i', [0])
+        self._stop = array.array('i', [0])
+        self._running = array.array('i', [0])
+        self._setup_push(display)
+
+    def _setup_push(self, disp):
+        sd = disp._spi_dma
+        if sd is None:
+            raise RuntimeError('async SPI push needs RP2 DMA')
+        base = uctypes.addressof(sd._dma.registers)
+        self._dma_read = base + 0x00
+        self._dma_write = base + 0x04
+        self._dma_count = base + 0x08
+        self._dma_ctrl = base + 0x0c
+        self._ctrl_word = sd._ctrl
+        self._spi_dr = sd._dr
+        self._spi_sr = sd._sr
+        self._lb_a = uctypes.addressof(disp._linebuf)
+        self._lb_b = uctypes.addressof(disp._linebuf2)
+        self._bpp = disp._bpp
+        self._scale = disp._scale
+        self._lut = disp._lut
+        self._color_mode = disp.source_color_mode
+
+    def start(self):
+        if self._running[0]:
+            return
+        self._stop[0] = 0
+        self._running[0] = 1
+        _thread.start_new_thread(self._loop, ())
+
+    def render(self, fb, width, height, region):
+        pass
+
+    def set_backlight(self, b):
+        if b <= 0:
+            self.stop()
+        else:
+            self.start()
+        self.display.set_backlight(b)
+
+    @property
+    def frames_pushed(self):
+        return self._frames[0]
+
+    def stop(self):
+        self._stop[0] = 1
+        deadline = time.ticks_add(time.ticks_ms(), 2000)
+        while self._running[0] and time.ticks_diff(deadline, time.ticks_ms()) > 0:
+            time.sleep_ms(5)
+
+    def _loop(self):
+        disp = self.display
+        w = self.fb_width
+        h = self.fb_height
+        bpp = self._bpp
+        cm = self._color_mode
+        sc = self._scale
+        lut = self._lut
+        fb = self._fb
+        push_args = (self._dma_read, self._dma_write, self._dma_count,
+                     self._dma_ctrl, self._ctrl_word, self._spi_dr, self._spi_sr,
+                     w * bpp, self._lb_a, self._lb_b, fb, w, 0, w, h, lut)
+        while self._stop[0] == 0:
+            disp._set_region_window(0, 0, w, h)
+            disp._spi_ctrl.write_cmd(b"\x2c")
+            disp._spi_ctrl.start_data()
+            if sc == 1 and bpp == 3:
+                if cm == 'RGB332':
+                    core1_spi_push332_888(*push_args)
+                else:
+                    core1_spi_push565_888(*push_args)
+            else:
+                disp.render(fb, w, h, (0, 0, w, h))
+            disp._spi_ctrl.end_data()
+            self._frames[0] += 1
+        self._running[0] = 0
--- infodisplay/ili9488display.py
+++ infodisplay/ili9488display.py
@@ -3,7 +3,6 @@ from machine import SPI, Pin
 from ili9488 import ILI9488, REFLECT, PORTRAIT, USD
 from drawing import Drawing
 
-# Default display dimensions
 DEFAULT_WIDTH = 480
 DEFAULT_HEIGHT = 320
 
@@ -15,16 +14,13 @@ class ILI9488Display:
     def create(provider):
         config = provider['config'].get('display', {})
 
-        # Get configurable dimensions and scale
         display_width = config.get('width', DEFAULT_WIDTH)
         display_height = config.get('height', DEFAULT_HEIGHT)
         scale = config.get('scale', 1)
 
-        # Framebuffer dimensions are display dimensions divided by scale
         fb_width = display_width // scale
         fb_height = display_height // scale
 
-        # Waveshare default SPI init speed for 3.5 display
         spi = SPI(1, baudrate=40_000_000, polarity=0, phase=0, sck=Pin(10), mosi=Pin(11))
         dc = Pin(8, Pin.OUT, value=0)
         cs = Pin(9, Pin.OUT, value=1)
@@ -51,11 +47,15 @@ class ILI9488Display:
             source_color_mode=mode
         )
 
-        # Drawing uses reduced framebuffer dimensions
         drawing = Drawing(fb_width, fb_height, color_mode=mode)
-        drawing.set_driver(ili)
 
-        ili.set_backlight(1.0)  # Explicitly trigger hardware backlight
+        from asyncspidriver import AsyncSpiDriver
+        driver = AsyncSpiDriver(ili, drawing.framebuffer, fb_width, fb_height)
+        driver.start()
+        drawing.set_driver(driver)
+        print("ili9488: async core1 push enabled (%s)" % mode)
+
+        ili.set_backlight(1.0)
 
         provider['display'] = drawing
         return ILI9488Display(ili, drawing)
